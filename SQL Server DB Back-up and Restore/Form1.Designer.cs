96a1d3326ce2564fdacab4eca0ce75cfeadf17a4
Ovidiu <Ovidiu@LAPTOP-MJRNSH14>
2017-07-07 16:21:43 +0300

    Add project files.

---
+++ SQL Server DB Back-up and Restore/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace SQL_Server_DB_Back_up_and_Restore
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BtnConnect = new System.Windows.Forms.Button();
+            this.BtnDisconnect = new System.Windows.Forms.Button();
+            this.txtDataSource = new System.Windows.Forms.TextBox();
+            this.txtUserId = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboDatabases = new System.Windows.Forms.ComboBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BtnBackup = new System.Windows.Forms.Button();
+            this.BtnBrowse = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtBackupFileLoc = new System.Windows.Forms.TextBox();
+            this.txtRestoreFileLoc = new System.Windows.Forms.TextBox();
+            this.BtnRestore = new System.Windows.Forms.Button();
+            this.BtnDBFileBrowser = new System.Windows.Forms.Button();
+            this.btnUseCred = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnUseCred);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.txtUserId);
+            this.groupBox1.Controls.Add(this.txtDataSource);
+            this.groupBox1.Controls.Add(this.BtnDisconnect);
+            this.groupBox1.Controls.Add(this.BtnConnect);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(441, 109);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SQL Server Authentication";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data Source";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "User ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 14);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // BtnConnect
+            // 
+            this.BtnConnect.Location = new System.Drawing.Point(332, 42);
+            this.BtnConnect.Name = "BtnConnect";
+            this.BtnConnect.Size = new System.Drawing.Size(89, 23);
+            this.BtnConnect.TabIndex = 3;
+            this.BtnConnect.Text = "Connect";
+            this.BtnConnect.UseVisualStyleBackColor = true;
+            this.BtnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // BtnDisconnect
+            // 
+            this.BtnDisconnect.Location = new System.Drawing.Point(332, 71);
+            this.BtnDisconnect.Name = "BtnDisconnect";
+            this.BtnDisconnect.Size = new System.Drawing.Size(89, 23);
+            this.BtnDisconnect.TabIndex = 4;
+            this.BtnDisconnect.Text = "Disconnect";
+            this.BtnDisconnect.UseVisualStyleBackColor = true;
+            this.BtnDisconnect.Click += new System.EventHandler(this.BtnDisconnect_Click);
+            // 
+            // txtDataSource
+            // 
+            this.txtDataSource.Location = new System.Drawing.Point(98, 14);
+            this.txtDataSource.Name = "txtDataSource";
+            this.txtDataSource.Size = new System.Drawing.Size(217, 22);
+            this.txtDataSource.TabIndex = 5;
+            // 
+            // txtUserId
+            // 
+            this.txtUserId.Location = new System.Drawing.Point(98, 42);
+            this.txtUserId.Name = "txtUserId";
+            this.txtUserId.Size = new System.Drawing.Size(217, 22);
+            this.txtUserId.TabIndex = 6;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(98, 69);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(217, 22);
+            this.txtPassword.TabIndex = 7;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboDatabases);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 128);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(441, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "DataBase Selection";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 14);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Database";
+            // 
+            // comboDatabases
+            // 
+            this.comboDatabases.FormattingEnabled = true;
+            this.comboDatabases.Location = new System.Drawing.Point(83, 25);
+            this.comboDatabases.Name = "comboDatabases";
+            this.comboDatabases.Size = new System.Drawing.Size(338, 22);
+            this.comboDatabases.TabIndex = 1;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtBackupFileLoc);
+            this.groupBox3.Controls.Add(this.BtnBackup);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.BtnBrowse);
+            this.groupBox3.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(12, 235);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(441, 100);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "DataBase Backup";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 14);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Destination";
+            // 
+            // BtnBackup
+            // 
+            this.BtnBackup.Location = new System.Drawing.Point(332, 56);
+            this.BtnBackup.Name = "BtnBackup";
+            this.BtnBackup.Size = new System.Drawing.Size(89, 23);
+            this.BtnBackup.TabIndex = 9;
+            this.BtnBackup.Text = "Backup";
+            this.BtnBackup.UseVisualStyleBackColor = true;
+            this.BtnBackup.Click += new System.EventHandler(this.BtnBackup_Click);
+            // 
+            // BtnBrowse
+            // 
+            this.BtnBrowse.Location = new System.Drawing.Point(332, 27);
+            this.BtnBrowse.Name = "BtnBrowse";
+            this.BtnBrowse.Size = new System.Drawing.Size(89, 23);
+            this.BtnBrowse.TabIndex = 8;
+            this.BtnBrowse.Text = "Browse";
+            this.BtnBrowse.UseVisualStyleBackColor = true;
+            this.BtnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.BtnRestore);
+            this.groupBox4.Controls.Add(this.txtRestoreFileLoc);
+            this.groupBox4.Controls.Add(this.BtnDBFileBrowser);
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox4.Location = new System.Drawing.Point(12, 342);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(442, 108);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "DataBase Restore";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 49);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 14);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Backup Path";
+            // 
+            // txtBackupFileLoc
+            // 
+            this.txtBackupFileLoc.Location = new System.Drawing.Point(98, 39);
+            this.txtBackupFileLoc.Name = "txtBackupFileLoc";
+            this.txtBackupFileLoc.Size = new System.Drawing.Size(217, 22);
+            this.txtBackupFileLoc.TabIndex = 10;
+            // 
+            // txtRestoreFileLoc
+            // 
+            this.txtRestoreFileLoc.Location = new System.Drawing.Point(98, 41);
+            this.txtRestoreFileLoc.Name = "txtRestoreFileLoc";
+            this.txtRestoreFileLoc.Size = new System.Drawing.Size(217, 22);
+            this.txtRestoreFileLoc.TabIndex = 11;
+            // 
+            // BtnRestore
+            // 
+            this.BtnRestore.Location = new System.Drawing.Point(332, 58);
+            this.BtnRestore.Name = "BtnRestore";
+            this.BtnRestore.Size = new System.Drawing.Size(89, 23);
+            this.BtnRestore.TabIndex = 12;
+            this.BtnRestore.Text = "Restore";
+            this.BtnRestore.UseVisualStyleBackColor = true;
+            this.BtnRestore.Click += new System.EventHandler(this.BtnRestore_Click);
+            // 
+            // BtnDBFileBrowser
+            // 
+            this.BtnDBFileBrowser.Location = new System.Drawing.Point(332, 29);
+            this.BtnDBFileBrowser.Name = "BtnDBFileBrowser";
+            this.BtnDBFileBrowser.Size = new System.Drawing.Size(89, 23);
+            this.BtnDBFileBrowser.TabIndex = 11;
+            this.BtnDBFileBrowser.Text = "Browse";
+            this.BtnDBFileBrowser.UseVisualStyleBackColor = true;
+            this.BtnDBFileBrowser.Click += new System.EventHandler(this.BtnDBFileBrowser_Click);
+            // 
+            // btnUseCred
+            // 
+            this.btnUseCred.Location = new System.Drawing.Point(321, 13);
+            this.btnUseCred.Name = "btnUseCred";
+            this.btnUseCred.Size = new System.Drawing.Size(114, 23);
+            this.btnUseCred.TabIndex = 8;
+            this.btnUseCred.Text = "Use Win Cred";
+            this.btnUseCred.UseVisualStyleBackColor = true;
+            this.btnUseCred.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 462);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "SQL Server Database Backup and Restore";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUserId;
+        private System.Windows.Forms.TextBox txtDataSource;
+        private System.Windows.Forms.Button BtnDisconnect;
+        private System.Windows.Forms.Button BtnConnect;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboDatabases;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtBackupFileLoc;
+        private System.Windows.Forms.Button BtnBackup;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button BtnBrowse;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button BtnRestore;
+        private System.Windows.Forms.TextBox txtRestoreFileLoc;
+        private System.Windows.Forms.Button BtnDBFileBrowser;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnUseCred;
+    }
+}
+
---
+++ SQL Server DB Back-up and Restore/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace SQL_Server_DB_Back_up_and_Restore
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection conn;
+        private SqlCommand command;
+        private SqlDataReader reader;
+        string sql = "";
+        string connectionString = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+              
+                //"Server= localhost; Database= employeedetails; Integrated Security=True;"
+                connectionString = @"Data Source=LAPTOP-MJRNSH14\SQLEXPRESS;Initial Catalog=Stock;Integrated Security=True";
+                   // "Data Source = " + txtDataSource.Text + "; User Id = " + txtUserId.Text + "; Password = " + txtPassword.Text + "";
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+                //sql = "EXEC sp_databases";
+                sql = "select * from sys.databases WHERE name NOT IN ('master', 'tempdb', 'model', 'msdb');";
+                command = new SqlCommand(sql, conn);
+                reader = command.ExecuteReader();
+                comboDatabases.Items.Clear();
+                
+                while (reader.Read())
+                {
+
+                    comboDatabases.Items.Add(reader[0].ToString());
+                }
+                reader.Dispose();
+                conn.Close();
+                conn.Dispose();
+
+                txtDataSource.Enabled = false;
+                txtUserId.Enabled = false;
+                txtPassword.Enabled = false;
+                BtnConnect.Enabled = false;
+                BtnDisconnect.Enabled = true;
+                comboDatabases.Enabled = true;
+                BtnBackup.Enabled = true;
+                BtnRestore.Enabled = true;
+             
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void BtnDisconnect_Click(object sender, EventArgs e)
+        {
+            txtDataSource.Enabled = true;
+            txtUserId.Enabled = true;
+            txtPassword.Enabled = true;
+            comboDatabases.Enabled = false;
+            BtnBackup.Enabled = false;
+            BtnRestore.Enabled = false;
+            BtnConnect.Enabled = true;
+            BtnDisconnect.Enabled = false;
+            btnUseCred.Enabled = true;
+    
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BtnDisconnect.Enabled = false;
+            comboDatabases.Enabled = false;
+            BtnBackup.Enabled = false;
+            BtnRestore.Enabled = false;
+        }
+
+        private void BtnBackup_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboDatabases.Text.CompareTo("")==0)
+                {
+                    MessageBox.Show("Te rog selecteaza o baza de date");
+                    return;
+                }
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+                sql = "BACKUP DATABASE "+comboDatabases.Text+" TO DISK ='" +txtBackupFileLoc.Text+ "\\" +comboDatabases.Text+  ".bak'";
+                command = new SqlCommand(sql,conn);
+                command.ExecuteNonQuery();
+                conn.Close();
+                conn.Dispose();
+                MessageBox.Show("Backup realizat cu succes!");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog()==DialogResult.OK)
+            {
+                txtBackupFileLoc.Text = dlg.SelectedPath;
+            }
+        }
+
+        private void BtnDBFileBrowser_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Backup Files(*.bak)|*bak|All Files(*.*)|*.*";
+            dlg.FilterIndex = 0;
+            if (dlg.ShowDialog()==DialogResult.OK)
+            {
+                txtRestoreFileLoc.Text = dlg.FileName;
+            }
+        }
+
+        private void BtnRestore_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboDatabases.Text.CompareTo("")==0)
+                {
+                    MessageBox.Show("te rog selecteaza o baza de date");
+                    return;
+                }
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+                sql = "ALTER DATABASE " + comboDatabases.Text + " SET SINGLE_USER WITH ROLLBACK IMMEDIATE;";
+                sql += "Restore database " + comboDatabases.Text + " FROM DISK = '" + txtRestoreFileLoc.Text + "' WITH REPLACE;";
+                command = new SqlCommand(sql, conn);
+                command.ExecuteNonQuery();
+                conn.Close();
+                conn.Dispose();
+                MessageBox.Show("baza de date restaurata cu succes");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connectionString = @"Data Source=LAPTOP-MJRNSH14\SQLEXPRESS;Initial Catalog=Stock;Integrated Security=True";
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+                sql = "select * from sys.databases WHERE name NOT IN ('master', 'tempdb', 'model', 'msdb');";
+                command = new SqlCommand(sql, conn);
+                reader = command.ExecuteReader();
+                comboDatabases.Items.Clear();
+
+                while (reader.Read())
+                {
+
+                    comboDatabases.Items.Add(reader[0].ToString());
+                }
+                reader.Dispose();
+                conn.Close();
+                conn.Dispose();
+
+                txtDataSource.Enabled = false;
+                txtUserId.Enabled = false;
+                txtPassword.Enabled = false;
+                BtnConnect.Enabled = false;
+                BtnDisconnect.Enabled = true;
+                comboDatabases.Enabled = true;
+                BtnBackup.Enabled = true;
+                BtnRestore.Enabled = true;
+                btnUseCred.Enabled = false;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
